e93133ee1de23be38c58f618e61b012a785a506f
vroque <vroque88@gmail.com>
2021-03-30 11:42:49 -0500

    add action lines and modify quotation marks

--- main.js
+++ main.js
@@ -1,25 +1,31 @@
 console.log('Welcome to the famous movie line generator!');
 
-const horrorLine1 = "Here's Johnny! -The Shining";
-const horrorLine2 = "Hello Clarice. -Silence of the Lambs";
-const horrorLine3 = "Do you like scary movies? -Scream";
-const horrorLine4 = "I see dead people. -The 6th Sense";
-const horrorLine5 = "What an excellent day for an exorcism. -The Exorcist";
+const horrorLine1 = '"Here\'s Johnny!" -The Shining';
+const horrorLine2 = '"Hello Clarice." -Silence of the Lambs';
+const horrorLine3 = '"Do you like scary movies?" -Scream';
+const horrorLine4 = '"I see dead people." -The 6th Sense';
+const horrorLine5 = '"What an excellent day for an exorcism." -The Exorcist';
 const horrorArray = [ horrorLine1, horrorLine2, horrorLine3, horrorLine4, horrorLine5];
 
-const comedyLine1 = "You're my boy, Blue! - Old School";
-const comedyLine2 = "Kill the Prime Minister of Malaysia! - Zoolander";
-const comedyLine3 = "This one time, at band camp... - American Pie";
-const comedyLine4 = "The dude abides. - The Big Lebowski";
-const comedyLine5 = "O'Doyle rules! - Billy Madison";
+const comedyLine1 = '"You\'re my boy, Blue!" - Old School';
+const comedyLine2 = '"Kill the Prime Minister of Malaysia!" - Zoolander';
+const comedyLine3 = '"This one time, at band camp..." - American Pie';
+const comedyLine4 = '"The dude abides." - The Big Lebowski';
+const comedyLine5 = '"O\'Doyle rules!" - Billy Madison';
 const comedyArray = [ comedyLine1,comedyLine2,comedyLine3,comedyLine4,comedyLine5];
 
-const dramaLine1 = "I'm going to make him an offer he can't refuse. - The Godfather";
-const dramaLine2 = "Toto, I've got a feeling we're not in Kansas anymore. -The Wizard of Oz";
-const dramaLine3 = "May the Force be with you. - Star Wars";
-const dramaLine4 = "You talking to me? - Taxi Driver";
-const dramaLine5 = "E.T. phone home. - E.T. The Extra-terrestrial";
+const dramaLine1 = '"I\'m going to make him an offer he can\'t refuse." - The Godfather';
+const dramaLine2 = '"Toto, I\'ve got a feeling we\'re not in Kansas anymore." -The Wizard of Oz';
+const dramaLine3 = '"May the Force be with you." - Star Wars';
+const dramaLine4 = '"You talking to me?" - Taxi Driver';
+const dramaLine5 = '"E.T. phone home." - E.T. The Extra-terrestrial';
 const dramaArray = [dramaLine1,dramaLine2,dramaLine3,dramaLine4,dramaLine5];
+
+const actionLine1 = '"Are you not entertained?" -Gladiator';
+const actionLine2 = '"Hasta la vista,baby" -Terminator 2: Judgment Day';
+const actionLine3 = '"This is Sparta!" -300';
+const actionLine4 = '"I\'m tired of these motherf***ing snakes, on this motherf***ing plane!" - Snakes on a Plane';
+const actionLine5 = '"Say hello to my little friend." -Scarface';
 
 
 
