23b8c9a300765abee1c22af3b7f7b9ebfcf36b2f
vroque <vroque88@gmail.com>
2021-03-29 19:13:38 -0500

    Add horror and comedy lines

---
+++ main.js
@@ -0,0 +1,17 @@
+console.log('Welcome to the famous movie line generator!');
+
+const horrorLine1 = "Here's Johnny! -The Shining";
+const horrorLine2 = "Hello Clarice. -Silence of the Lambs";
+const horrorLine3 = "Do you like scary movies? -Scream";
+const horrorLine4 = "I see dead people. -The 6th Sense";
+const horrorLine5 = "What an excellent day for an exorcism. -The Exorcist";
+const horrorArray = [ horrorLine1, horrorLine2, horrorLine3, horrorLine4, horrorLine5];
+
+const comedyLine1 = "You're my boy, Blue! - Old School";
+const comedyLine2 = "Kill the Prime Minister of Malaysia! - Zoolander";
+const comedyLine3 = "This one time, at band camp... - American Pie";
+const comedyLine4 = "The dude abides. - The Big Lebowski";
+const comedyLine5 = "O'Doyle rules! - Billy Madison";
+const comedyArray = [ comedyLine1,comedyLine2,comedyLine3,comedyLine4,comedyLine5];
+
+
